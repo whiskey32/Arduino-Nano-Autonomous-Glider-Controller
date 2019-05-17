567f0adbf98d77c9e7462936e18c126443b7059c
whiskey32 <50754647+whiskey32@users.noreply.github.com>
2019-05-17 23:32:09 +0200

    Add files via upload

--- autonomous_glider_controller_0.1/autonomous_glider_controller_0.1.ino
+++ autonomous_glider_controller_0.1/autonomous_glider_controller_0.1.ino
@@ -1,9 +1,9 @@
 void setup() {
-  // put your setup code here, to run once:
+  // put your setup code here, to run o22222222222222222222nce:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // put your main code here, to run repe222222222222222222222222atedly:
 
 }
