3f7cc772568b720fab43501c4ac88016c189b59b
whiskey32 <50754647+whiskey32@users.noreply.github.com>
2019-07-25 19:42:23 +0200

    Add files via upload
    PWM measure code with library and PWM measure code without library. Both libraries contain an potentiometer application that can generate different PWM pulse widths.

--- Tests/pwm/pwm/pwm.ino
+++ Tests/pwm/pwm/pwm.ino
@@ -12,58 +12,71 @@ volatile unsigned long ulStartPeriod = 0; // set in the interrupt
 volatile boolean b_OH_SHIT_SWITCH_Signal = false; // set in the interrupt and read in the loop
 
 // Servo Objects for PWM Signal Generation
-ServoTimer2 rudder;
-ServoTimer2 elevator;
+//ServoTimer2 rudder;
+//ServoTimer2 elevator;
+
+
+// Control Servo Signal with potentiometer
+ServoTimer2 myservo;  // create servo object to control a servo
+int potpin = 0;  // analog pin used to connect the potentiometer
+int val;    // variable to read the value from the analog pin
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   attachInterrupt(digitalPinToInterrupt(3), pwm_calc, CHANGE);
 
-  rudder.attach(A1);     // PWM Rudder Output. Pin A1
-  elevator.attach(A2);   // PWM Elevator Output. Pin A2 
-  
-  pinMode(4, OUTPUT);    // Sets digital pin 4 as Signal Select switch output signal 
-  
+  // rudder.attach(A1);     // PWM Rudder Output. Pin A1
+  // elevator.attach(A2);   // PWM Elevator Output. Pin A2
+
+  // pinMode(4, OUTPUT);    // Sets digital pin 4 as Signal Select switch output signal
+
+  // Control Servo Signal with potentiometer
+  myservo.attach(3);  // attaches the servo on pin 3 to the servo object
+
 }
 
 void loop() {
-  
+
   // put your main code here, to run repeatedly:
-  rudder.write(1000); 
-  elevator.write(1500); // Set servo to mid-point
+  //  rudder.write(1000);
+  //  elevator.write(1500); // Set servo to mid-point
+  // digitalWrite(4, LOW); // Signal select LOW = ARDUINO PWM Input; HIGH = ONBOARD RECEIVER PWM Input
 
- digitalWrite(4, LOW); // Signal select LOW = ARDUINO PWM Input; HIGH = ONBOARD RECEIVER PWM Input
+  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
+  val = map(val, 0, 1023, 0, 4000);     // scale it to use it with the servo (value between 0 and 180)
+  myservo.write(val);                  // sets the servo position according to the scaled value
+  // Serial.println(val);
 
- if(b_OH_SHIT_SWITCH_Signal)
- {
-  if ( n_OH_SHIT_SWITCH_In >= 1000 &&  n_OH_SHIT_SWITCH_In <= 3000)
+  if (b_OH_SHIT_SWITCH_Signal)
   {
-   Serial.println(n_OH_SHIT_SWITCH_In);  
-    // Set switch back to false to recalculate next PWM duaration
- }
- b_OH_SHIT_SWITCH_Signal = false;
-}
+    if ( n_OH_SHIT_SWITCH_In >= 1000 &&  n_OH_SHIT_SWITCH_In <= 3000)
+    {
+      Serial.println(n_OH_SHIT_SWITCH_In);
+      // Set switch back to false to recalculate next PWM duaration
+    }
+    b_OH_SHIT_SWITCH_Signal = false;
+  }
 }
 
 void pwm_calc()
 {
   // if the pin is high, its the start of an interrupt
-  if(digitalRead(OH_SHIT_SWITCH_IN_PIN) == HIGH)
-  { 
+  if (digitalRead(OH_SHIT_SWITCH_IN_PIN) == HIGH)
+  {
     // get the time using micros
     ulStartPeriod = micros();
   }
   else
   {
-    // if the pin is low, its the falling edge of the pulse so now we can calculate the pulse duration by subtracting the 
+    // if the pin is low, its the falling edge of the pulse so now we can calculate the pulse duration by subtracting the
     // start time ulStartPeriod from the current time returned by micros()
-    if(ulStartPeriod && (b_OH_SHIT_SWITCH_Signal == false))
+    if (ulStartPeriod && (b_OH_SHIT_SWITCH_Signal == false))
     {
       n_OH_SHIT_SWITCH_In = (int)(micros() - ulStartPeriod);
       ulStartPeriod = 0;
       // bNewThrottleSignal back to false
-     b_OH_SHIT_SWITCH_Signal = true;
+      b_OH_SHIT_SWITCH_Signal = true;
     }
   }
 }
--- Tests/pwm_with_library/pwm_with_library/pwm_with_library.ino
+++ Tests/pwm_with_library/pwm_with_library/pwm_with_library.ino
@@ -1,8 +1,8 @@
 #include <PinChangeInt.h>
 #include <Servo.h>
- 
+
 #define MY_PIN 3 // we could choose any pin
- 
+
 volatile int pwm_value = 0;
 volatile int prev_time = 0;
 uint8_t latest_interrupted_pin;
@@ -10,35 +10,35 @@ uint8_t latest_interrupted_pin;
 // Servo Objects for PWM Signal Generation
 Servo rudder;
 Servo elevator;
- 
+
 void rising()
 {
-  latest_interrupted_pin=PCintPort::arduinoPin;
+  latest_interrupted_pin = PCintPort::arduinoPin;
   PCintPort::attachInterrupt(latest_interrupted_pin, &falling, FALLING);
-  prev_time = micros();s
+  prev_time = micros();
 }
- 
+
 void falling() {
-  latest_interrupted_pin=PCintPort::arduinoPin;
+  latest_interrupted_pin = PCintPort::arduinoPin;
   PCintPort::attachInterrupt(latest_interrupted_pin, &rising, RISING);
-  pwm_value = micros()-prev_time;
+  pwm_value = micros() - prev_time;
   Serial.println(pwm_value);
 }
- 
+
 void setup() {
   pinMode(MY_PIN, INPUT); digitalWrite(MY_PIN, HIGH);
   Serial.begin(9600);
   PCintPort::attachInterrupt(MY_PIN, &rising, RISING);
-   rudder.attach(A1);     // PWM Rudder Output. Pin A1
-  elevator.attach(A2);   // PWM Elevator Output. Pin A2 
-  
-  pinMode(4, OUTPUT);    // Sets digital pin 4 as Signal Select switch output signal 
+  // rudder.attach(A1);     // PWM Rudder Output. Pin A1
+  // elevator.attach(A2);   // PWM Elevator Output. Pin A2
+
+  //  pinMode(4, OUTPUT);    // Sets digital pin 4 as Signal Select switch output signal
 }
- 
+
 void loop() {
-    rudder.writeMicroseconds(1000); 
-  elevator.writeMicroseconds(1500); // Set servo to mid-point
+  //  rudder.writeMicroseconds(1000);
+  //elevator.writeMicroseconds(1500); // Set servo to mid-point
 
 
- digitalWrite(4, LOW); // Signal select LOW = ARDUINO PWM Input; HIGH = ONBOARD RECEIVER PWM Input
+  //digitalWrite(4, LOW); // Signal select LOW = ARDUINO PWM Input; HIGH = ONBOARD RECEIVER PWM Input
 }
