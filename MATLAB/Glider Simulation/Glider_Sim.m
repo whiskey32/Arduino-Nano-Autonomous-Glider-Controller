bb92bc98728f36b8c50dcd9dc1a8be091fe7376c
whiskey32 <50754647+whiskey32@users.noreply.github.com>
2019-07-30 20:44:58 +0200

    MATLAB Docs
    Contains plot graph matlab code. First code for simulation added as well

---
+++ MATLAB/Glider Simulation/Glider_Sim.m
@@ -0,0 +1,94 @@
+%  Dynamic Trajectory Control of Glider
+% 
+%  This code simulates the dynamic trajectory control of a glider. The
+%  aerodynamic coefficient model is similar to that of the simulation-based
+%  performance study of R/C model sailplanes by Beron-Rawdon. The model
+%  here uses values of parameters from the Beron-Rawdon's paper and
+%  formulas for the construction of aerodynamic coefficients include most
+%  of the constitutive elements of that model. The drag and lift forces are
+%  functions of angle of attack and certain design parameters. 
+%  A new dynamic control alogorithm from the Rui Dilao paper runs
+%  iteratively and the approach to the target point is self-correcting.
+%  
+%  
+% // Created: BJGW DU PLESSIS
+% // Student Number: 18989780 
+% // Modified: 2019/07/30
+% // Version: 0.1
+
+
+%/////////////////////////////////////////////////////////////////////////
+%// Aerodynamic Force Coefficient Parameters
+%/////////////////////////////////////////////////////////////////////////
+
+% Glider Lift Coefficient Variables
+a_0 = 0.1 * (180/(pi));      % Lift Curve Slope
+e = 0.95;                    % Oswald Efficiency Factor
+AR = 11.08;                  % Aspect Ratio
+alpha_0 = -2.5 * (pi/(180)); % Zero Point
+
+% Glider Drag Coefficient Variables
+l = 102.4;                   % Wingspan Length
+l_t = 0.28 * l;              % Fuselage Moment Arm Length
+V_H = 0.4;                   % Horizontal Tail Volume Ratio
+V_V = 0.02;                  % Vertical Tail Volume Ratio
+c_bar = (1.03*l)/(AR);       % Mean Aerodynamic Chord
+CD_F = 0.008;                % Fuselage Drag Coefficient
+CD_T = 0.01;                 % Tail Drag Coefficient
+CD_E = 0.002;                % Miscellaneous "Extra Drag" Coefficient
+S_F = 216;                   % Fuselage Area
+S = (l^2)/(AR);              % Wing Surface Area 
+S_T = (V_H*c_bar*S)/(l_t);   % Horizontal Tail Surface Area
+S_V = (V_V*l*S)/(l_t);       % Vertical Tail Surface Area
+Cd_0 = 0.01;                 % Wing Profile Drag Variable 1
+Cd_L = 0.05;                 % Wing Profile Drag Variable 2
+CL_min = 0.4;                % Wing Profile Drag Variable 3
+
+
+%/////////////////////////////////////////////////////////////////////////
+%// Force Formulas to Construct Aerodynamic Coefficients 
+%/////////////////////////////////////////////////////////////////////////
+
+% Glider Lift Formula
+ CL_alpha = (a_0)/(1+(a_0)/(pi*e*AR));                          % Finite Wing Slope
+
+ 
+ % Drag Coefficient function of alpha
+ CD_0 = (CD_F*S_F)/(S) + (CD_T*(S_T + S_V))/(S) + CD_E + Cd_0;  % Constant Part of Total Drag Formula
+ 
+ CD = CD_0 + Cd_L*( LC(alpha*(pi)/(180),CL_alpha,alpha_0) - CL_min).^2 + ((LC(alpha*(pi)/(180),CL_alpha,alpha_0)).^2)/(pi*e*AR);
+  
+ CL_div_CD = LC(alpha*(pi)/(180),CL_alpha,alpha_0)./CD;
+
+%/////////////////////////////////////////////////////////////////////////
+%// Plot Graphs
+%/////////////////////////////////////////////////////////////////////////
+
+d_alpha= 0.01;
+alpha = -45:d_alpha:45;  % Plot in radians up until 45 degrees
+ 
+% figure(1)
+% plot(alpha,LC(alpha*(pi)/(180),CL_alpha,alpha_0));
+% xlabel('alpha (deg)');
+% ylabel('CL');
+% figure(2)
+% plot(alpha,CD);
+% xlabel('alpha (deg)');
+% ylabel('CD');
+% figure(3)
+ plot(alpha,CL_div_CD);
+ grid on
+ title('CL/CD Ratio vs Angle of Attack ');
+ xlabel('alpha (deg)');
+ ylabel('CL/CD');
+
+%/////////////////////////////////////////////////////////////////////////
+%// Functions
+%/////////////////////////////////////////////////////////////////////////
+
+% Lift Coefficient which is a function of alpha
+function CL = LC(alpha,CL_alpha,alpha_0)
+    CL = CL_alpha*(alpha - alpha_0);  
+end
+
+
---
+++ MATLAB/PLOT GRAPHS/draw_graph.m
@@ -0,0 +1,131 @@
+%  Graph Plot Code
+%
+%  This code generates graphs from the measurement file created by the Arduino nano saved as .txt file.
+%
+% // Created: BJGW DU PLESSIS
+% // Student Number: 18989780
+% // Modified: 2019/07/16
+% // Version: 0.1
+
+clear all;
+close all;                   
+clc
+
+
+% Read in .tx.t file to array
+ IMU_data=dlmread('complementary_filter_pitch_a01.csv');
+% Mag_data=dlmread('raw_mag_outside_balcony.csv');
+% Mag_data=dlmread('coetzenburg_mag_3.csv');
+
+
+% Copy Accelerometer data to inidividual axis array
+% a_x=IMU_data(:,1);
+% a_y=IMU_data(:,2);
+% a_z=IMU_data(:,3);
+
+% Copy Gyroscope data to inidividual axis array
+% g_x=IMU_data(:,1);
+% g_y=IMU_data(:,2);
+% g_z=IMU_data(:,3);
+
+% Plot Accelerometer Graphs
+% [n,p] = size(a_x);
+% t= 1:n;
+% figure(1);
+% plot(t,a_x);
+% hold on
+% plot(t,a_y);
+% plot(t,a_z);
+% legend('a_x','a_y','a_z');
+% xlabel('Samples'), ylabel('Acceleration (g)')
+% title('Accelerometer Data ')
+% hold off
+
+% Plot Gyroscope Graphs and get mean
+% [n,p] = size(g_x);
+% t= 1:n;
+% figure(1);
+% plot(t,g_x);
+% hold on
+% plot(t,g_y);
+% plot(t,g_z);
+% legend('g_x','g_y','g_z');
+% xlabel('Samples'), ylabel('Angular velocity (dps)')
+% title('Gyroscope Data ')
+% hold off
+% 
+% Mean_gx = mean(g_x)
+% Mean_gy = mean(g_y)
+% Mean_gz = mean(g_z)
+
+% Plot Yaw Graph
+% figure(3);
+% plot(t,IMU_data(:,7));
+% xlabel('Samples'), ylabel('Yaw (degrees)')
+% title('Yaw ')
+
+% Copy Magnetometer data to inidividual axis array
+% m_x=Mag_data(:,1);
+% m_y=Mag_data(:,2);
+% m_z=Mag_data(:,3);
+% 
+% % m_x2=Mag_data2(:,1);
+% % m_y2=Mag_data2(:,2);
+% % m_z2=Mag_data2(:,3);
+% % % 
+% % Plot Magnetometer 3D Scatter Plot 
+% figure(1);
+% scatter3(m_x,m_y,m_z);
+
+% figure(2);
+% scatter3(m_x2,m_y2,m_z2);
+
+%#######################################################################%
+% Copy Filtered and Non Filtered Roll test data data to inidividual axis array
+roll_nf=IMU_data(:,2);
+roll_f=IMU_data(:,4);
+
+% Plot Filtered and Non Filtered Roll test Plot 
+[n,p] = size(roll_nf);
+t= 1:n;
+figure(1);
+plot(t,roll_nf);
+hold on
+plot(t,roll_f);
+legend('roll_nf','roll_f');
+xlabel('Samples'), ylabel('Roll (degrees)')
+title('Complementary Filter on Roll data ')
+hold off
+
+% 
+% Copy Filtered and Non Filtered Pitch test data data to inidividual axis array
+% pitch_nf=IMU_data(:,1);
+% pitch_f=IMU_data(:,3);
+% 
+% Plot Filtered and Non Filtered Pitch test Plot 
+% figure(2);
+% plot(t,pitch_nf);
+% hold on
+% plot(t,pitch_f);
+% legend('pitch_nf','pitch_f');
+% xlabel('Samples'), ylabel('Pitch (degrees)')
+% title('Complementary Filter on Pitch data ')
+% hold off
+
+%#######################################################################%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
